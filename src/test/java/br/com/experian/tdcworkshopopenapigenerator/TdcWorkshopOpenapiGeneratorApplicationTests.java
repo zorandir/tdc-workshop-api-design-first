6da2d048a740fe2499c06772f823cdfbed2ba607
Zorandir <zorandir.junior@br.experian.com>
2023-09-17 16:04:03 -0300

    first commit

---
+++ src/test/java/br/com/experian/tdcworkshopopenapigenerator/TdcWorkshopOpenapiGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.experian.tdcworkshopopenapigenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdcWorkshopOpenapiGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
